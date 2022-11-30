dc1b1cf930c2ede5937222f0a800ae5e81587ce8
lianxinbo <lianxinbo@hengli.com>
2022-11-30 11:04:07 +0800

    RenderProps 优化

--- src/Module/RenderProps/RenderProps.tsx
+++ src/Module/RenderProps/RenderProps.tsx
@@ -1,14 +1,27 @@
 import React, { Component } from "react";
 
 export default class RenderPropsDemo extends Component {
+  renderCat = (point: RenderPropsState) => {
+    return <Cat {...point} />;
+  };
+
   render() {
     return (
       <div>
-        <MouseTracker
-          render={(state) => {
-            return <Cat {...state} />;
-          }}
-        />
+        {
+            /*
+            在这样例子中，每次 <MouseTracker> 渲染，
+            它会生成一个新的函数作为 <Mouse render> 的 prop，
+            因而在同时也抵消了继承自 React.PureComponent 的 <Mouse> 组件的效果！
+            <MouseTracker
+                render={(state) => {
+                    return <Cat {...state} />;
+                }}
+            />
+            为了绕过这一问题，有时你可以定义一个 prop 作为实例方法，类似这样：
+            */
+        }
+        <MouseTracker render={this.renderCat} />
       </div>
     );
   }
@@ -25,8 +38,7 @@ export class Cat extends Component<RenderPropsState> {
           width: "100px",
           height: "100px",
           backgroundColor: "red",
-        }}>
-      </div>
+        }}></div>
     );
   }
 }
