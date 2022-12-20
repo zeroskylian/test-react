3ccbd9eb876e0590a5872cb28a161a3df2dcbb76
lianxinbo <lianxinbo@hengli.com>
2022-12-20 18:05:31 +0800

    release

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-// import RouterTest from './components/MultiLevelRouter';
-import { NewTab } from './components/NewTab';
+import RouterTest from './components/MultiLevelRouter';
 
 export default class App extends Component {
   render() {
     return (
-      <NewTab />
-      // <RouterTest />
+      <RouterTest />
       // <GridTest />
     );
   }
